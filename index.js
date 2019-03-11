255994238a58685394102d8ececb498388a919fb
Dominic Tremblay <dominictremblay24@gmail.com>
2019-03-10 21:41:27 -0400

    demo reset

--- index.js
+++
@@ -1,9 +0,0 @@
-var tvMazeApi = require('./show_api');
-
-tvMazeApi.getShowInfo(3022, function(showInfo) {
-  console.log(showInfo);
-});
-
-tvMazeApi.getCastList(3022, function(castInfo) {
-  console.log(castInfo);
-});
